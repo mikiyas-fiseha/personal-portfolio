3b561481b8b7abe085da145886fddb1a457de791
John Doe <mikiyasfiseha097@gmail.com>
2023-05-05 14:36:29 -0700

    link update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import { MailchimpForm } from "./MailchimpForm";
 import logo from '../assets/img/logo.png';
 
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import githubicon from '../assets/img/github.svg';
 
 export const Footer = () => {
   return (
@@ -18,10 +18,10 @@ export const Footer = () => {
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://github.com/mikiyas-fiseha"><img src={githubicon} alt="Icon" /></a>
               <a href="#"><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2023. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
