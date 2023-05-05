e0eee7b4dd80ad839c3add789ec59ee579c36975
John Doe <mikiyasfiseha097@gmail.com>
2023-05-05 14:58:31 -0700

    update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -48,7 +48,7 @@ export const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <p>Here, you'll find a summary of my skills and experience in various areas of web development and design. I've broken down my skills into several categories to make it easier for you to understand my areas of expertise..<br></br></p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={htmlicon} alt="Image" />
