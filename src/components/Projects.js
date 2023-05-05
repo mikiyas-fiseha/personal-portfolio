895549acfd71b634783e44cdaeed450945277748
John Doe <mikiyasfiseha097@gmail.com>
2023-05-05 14:08:17 -0700

    card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
+import projImg1 from "../assets/img/fastfood.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
@@ -14,7 +14,7 @@ export const Projects = () => {
       title: "Business Startup",
       description: "Design & Development",
       imgUrl: projImg1,
-      link:"https://www.google.com/"
+      link:"https://mikiyas-fiseha.github.io/Fast_Food_Website/"
     },
     {
       title: "Business Startup",
